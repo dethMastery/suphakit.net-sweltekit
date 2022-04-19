3c9073c0bf392761c6bbec8abef182407fb38064
dethMastery <owner@detzz.in.th>
2022-04-20 02:58:17 +0700

    updated: header

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,124 +1,62 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import logo from './svelte-logo.svg';
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
-
 	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
 		<ul>
-			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-			<li class:active={$page.url.pathname === '/about'}>
-				<a sveltekit:prefetch href="/about">About</a>
-			</li>
-			<li class:active={$page.url.pathname === '/todos'}>
-				<a sveltekit:prefetch href="/todos">Todos</a>
-			</li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav>
+			<a sveltekit:prefetch href="/">
+				<li>
+					home
+				</li>
+			</a>
 
-	<div class="corner">
-		<!-- TODO put something else here? github link? -->
-	</div>
+			<li></li>
+
+			<a sveltekit:prefetch href="https://blog.suphakit.net">
+				<li>
+					blog
+				</li>
+			</a>
+
+			<li></li>
+
+			<a sveltekit:prefetch href="/about">
+				<li class:active={$page.url.pathname === '/about'}>
+					about
+				</li>
+			</a>
+
+			<li></li>
+		</ul>
+	</nav>
 </header>
 
 <style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
 	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
+		width: 100%;
+		height: auto;
 	}
 
 	ul {
-		position: relative;
-		padding: 0;
 		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
+		padding: 0;
+		background-color: whitesmoke;
+		color: #2e2f2f;
+		text-align: right;
+		padding-top: 1rem;
+		padding-bottom: 1rem;
 	}
 
 	li {
-		position: relative;
-		height: 100%;
+		display: inline;
+		padding: .5rem;
+		padding-top: 1rem;
+		padding-bottom: 1rem;
+		background-color: whitesmoke;
 	}
 
-	li.active::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--accent-color);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 1em;
-		color: var(--heading-color);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--accent-color);
+	a li:hover {
+		background-color: rgb(182, 182, 182);
 	}
 </style>
