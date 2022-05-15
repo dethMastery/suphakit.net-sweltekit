9becc2872af96c8950a8a0b45f7fd10c6019bcd9
dethMastery <owner@detzz.in.th>
2022-05-15 21:35:38 +0700

    added: static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-
+import adapter from '@sveltejs/adapter-static';
+ 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
+ 
 	kit: {
-		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+	  
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
 		}
 	}
 };
-
+ 
 export default config;
